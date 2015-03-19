74a446fb941df15e039edf0fc34e53880991ef90
unknown <pabloconde88@gmail.com>
2015-03-19 17:54:41 -0300

    Fixed issue that prevent older phones from seing transparency

--- app/src/main/java/com/concon/talkabout/talkabout/MainMenu.java
+++ app/src/main/java/com/concon/talkabout/talkabout/MainMenu.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
