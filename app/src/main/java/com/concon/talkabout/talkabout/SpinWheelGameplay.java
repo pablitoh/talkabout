44d725183e73151d2953333a1b94781ace7e5254
Pablo Conde <pablo.conde@openenglish.com>
2015-04-09 12:51:15 -0300

    Added menu screen for wheel

--- app/src/main/java/com/concon/talkabout/talkabout/SpinWheelGameplay.java
+++ app/src/main/java/com/concon/talkabout/talkabout/SpinWheelGameplay.java
@@ -149,13 +149,6 @@ public class SpinWheelGameplay extends Activity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_activity_spinwheel_gameplay, menu);
-        return true;
-    }
-
     private class MyOnTouchListener implements View.OnTouchListener {
 
         private double startAngle;
---
+++ app/src/main/java/com/concon/talkabout/talkabout/SpinWheelMainMenu.java
@@ -0,0 +1,59 @@
+package com.concon.talkabout.talkabout;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.concon.talkabout.talkabout.ads.CustomInterstitial;
+import com.concon.talkabout.talkabout.analitycs.GoogleAnalyticsApp;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
+
+
+public class SpinWheelMainMenu extends Activity {
+
+    CustomInterstitial ad;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drinkingwheel_main_menu);
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        Tracker t = ((GoogleAnalyticsApp) getApplication()).getTracker(GoogleAnalyticsApp.TrackerName.APP_TRACKER);
+        t.setScreenName("SpinWheel Main Menu");
+        t.enableAdvertisingIdCollection(true);
+        t.send(new HitBuilders.AppViewBuilder().build());
+        ad = new CustomInterstitial(this);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        ad.showIfAvailable();
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleAnalytics.getInstance(this).reportActivityStart(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        GoogleAnalytics.getInstance(this).reportActivityStop(this);
+    }
+
+    public void startWheel(View v) {
+        Intent intent = new Intent(SpinWheelMainMenu.this, SpinWheelGameplay.class);
+        startActivity(intent);
+
+    }
+}
