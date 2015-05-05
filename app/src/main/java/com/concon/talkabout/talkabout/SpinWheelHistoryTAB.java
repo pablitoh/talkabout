724667a7472b34f7fbf9b41e0ef65120a4c228e4
unknown <pabloconde88@gmail.com>
2015-05-04 21:25:35 -0300

    Added nothing to show

--- app/src/main/java/com/concon/talkabout/talkabout/SpinWheelHistoryTAB.java
+++ app/src/main/java/com/concon/talkabout/talkabout/SpinWheelHistoryTAB.java
@@ -1,29 +1,36 @@
 package com.concon.talkabout.talkabout;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.concon.talkabout.talkabout.dataType.RewardCard;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 import adapters.CustomListAdapter;
 
 /**
  * Created by OE on 28/04/2015.
  */
-public class SpinWheelHistoryTAB extends Fragment {
+public class SpinWheelHistoryTAB extends ListFragment {
 
     // Defined Array values to show in ListView
     private ArrayList<RewardCard> lista = new ArrayList<>();
     ListView listView;
+    CustomListAdapter adapter;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        listView = getListView();
+        adapter = new CustomListAdapter(getActivity(), lista);
+        setListAdapter(adapter);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,23 +38,17 @@ public class SpinWheelHistoryTAB extends Fragment {
 
         View android = inflater.inflate(R.layout.spin_wheel_history_tab, container, false);
 
-        CustomListAdapter adapter = new CustomListAdapter(getActivity(),lista);
-        listView = (ListView) android.findViewById(R.id.historyList);
-        listView.setAdapter(adapter);
         return android;
     }
 
     public void addToList(RewardCard rewardCard) {
-        if(!rewardCard.cardTitle.equals(getString(R.string.cleanseTitle)))
-        {
+        if (!rewardCard.cardTitle.equals(getString(R.string.cleanseTitle))) {
             lista.add(rewardCard);
             Collections.reverse(lista);
-        }
-        else
-        {
+        } else {
             lista.clear();
         }
-        listView.invalidateViews();
+        adapter.notifyDataSetChanged();
     }
 
 
